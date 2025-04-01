3f74b6d197434e1a454309b7a10767c1847ea6a7
JaoVicy <filipimjoa@gmail.com>
2025-04-01 14:55:30 -0300

    Initial Commit

---
+++ DistanceCalcTests/DistanceCalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  DistanceCalcTests.swift
+//  DistanceCalcTests
+//
+//  Created by João Víctor Benetti Filipim on 01/04/25.
+//
+
+import Testing
+@testable import DistanceCalc
+
+struct DistanceCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
